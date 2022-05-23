5daa6709701e8a113028c25aa1147b0bd683a8a9
marivolvakova <104025325+marivolvakova@users.noreply.github.com>
2022-05-23 19:18:13 +0300

    Initial Commit

---
+++ VkPage_storybord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkPage_storybord
+//
+//  Created by Мария Вольвакова on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
